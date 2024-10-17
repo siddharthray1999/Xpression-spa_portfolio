cd5dac397174660f6d135a164c470756c42b8989
siddharth <siddharthray47@gmail.com>
2024-10-17 20:11:10 +0530

    navbar logo width change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   return (
     <nav className=" mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
-        <img src={sidlogo} alt="logo" className="" width={150} height={150} />
+        <img src={sidlogo} alt="logo" className="rounded-full" width={50} height={50} />
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl ">
         <FaLinkedin />
